7c2faacaf2221399fdcb63d1b4e7e6a68ebe394a
AlexForBora <schornberg@bora-computer.de>
2017-05-29 15:26:56 +0200

    f

--- engine/Shopware/Plugins/Local/Frontend/ToolBox/Views/frontend/_resources/javascript/jquery.konfigurator.js
+++ engine/Shopware/Plugins/Local/Frontend/ToolBox/Views/frontend/_resources/javascript/jquery.konfigurator.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function(){
 
     $('body').on('.collapse--header', 'click', function({
+        console.log($(this));
         $(this).next('.collapse--content').slideToggle();
     }));
-    
+
 })
