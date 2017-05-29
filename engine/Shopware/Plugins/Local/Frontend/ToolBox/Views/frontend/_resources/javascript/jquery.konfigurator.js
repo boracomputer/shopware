437dabd8e221ac30e707a02d4316ef94625e599f
AlexForBora <schornberg@bora-computer.de>
2017-05-29 16:38:41 +0200

    js fix

--- engine/Shopware/Plugins/Local/Frontend/ToolBox/Views/frontend/_resources/javascript/jquery.konfigurator.js
+++ engine/Shopware/Plugins/Local/Frontend/ToolBox/Views/frontend/_resources/javascript/jquery.konfigurator.js
@@ -1,8 +1,7 @@
-jQuery(document).ready(function(){
 
+;(function($, window, undefined) {
     $('body').on('click', '.collapse--header', function(){
         console.log($(this));
         $(this).next('.collapse--content').slideToggle();
     });
-
-})
+})(jQuery, window);
