8a1d4a55576bacbe8968eb36bbd1e466d537f277
AlexForBora <schornberg@bora-computer.de>
2017-07-18 15:05:35 +0200

    fix

--- themes/Frontend/DashOneV2Layout/frontend/_public/src/js/jquery.custom.js
+++ themes/Frontend/DashOneV2Layout/frontend/_public/src/js/jquery.custom.js
@@ -1,5 +1,5 @@
 $(function () {
-    
+
     $('body').on( 'click', '.js--toggle-trigger', function (e) {
         e.preventDefault();
         var t=$(this).attr('data-target');
@@ -14,5 +14,25 @@ $(function () {
         $(this).parents('.js--toggle-target').first().slideUp("fast", function(){
         });
         return false;
+    });
+
+    $('.product--box').on('click', '.action--quickview', function(e){
+        e.preventDefault();
+        var t = $(this);
+        $.loadingIndicator.open();
+        $.ajax({
+            url: t.attr('href'),
+            success: function (data) {
+                $.loadingIndicator.close(function(){
+                    var $d = $('<div/>', {
+                        html: data
+                    });
+                    $.modal.open( $d, {
+                        width: 800,
+                        title: t.attr('data-title'),
+                    });
+                });
+            }
+        });
     });
 });
